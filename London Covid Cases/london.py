4a7332272962cf1b67aa50f3d5091d8bf5ca031c
Giorgio Sala <78495487+giorgiosala@users.noreply.github.com>
2021-02-23 17:43:05 +0000

    London Covid Map

--- London Covid Cases/london.py
+++ London Covid Cases/london.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Python ex3
-
 Author: Giorgio Sala
 
 Objective:
@@ -9,14 +7,22 @@ This program reads a csv file of Coronavirus cases per london borough and
 displays the data in a shapefile of london.  
 
 
-Links:
+Libraries that must be installed to run the program:
 
-To run this file, geopandas and its dependencies must be downloaded and installed. to do that, follow the instructions on
-https://geopandas.org/install.html#creating-a-new-environment
+- Geopandas and its dependencies must be downloaded and installed. (https://geopandas.org/install.html#creating-a-new-environment)
+- Matplotlib
+- Pandas
+- Numpy
+- Pillow
+
+Mapclassify must be installed (in terminal: "pip install mapclassify")
 
 The CSV file was downloaded from:
-    
+https://data.london.gov.uk/download/coronavirus--covid-19--cases/151e497c-a16e-414e-9e03-9e428f555ae9/phe_cases_london_boroughs.csv
 The shapefile was downloaded from: 
+https://data.london.gov.uk/download/statistical-gis-boundary-files-london/08d31995-dd27-423c-a987-57fe8e952990/London-wards-2018.zip
+
+(QGIS 3.10 was used to merge all the districts of a borough in one single shape)
 
 
 """
@@ -33,11 +39,11 @@ image_frame=[]
 #Read the CSV file and privot the table to easier analisis
 district= pd.read_csv("phe_cases_london_boroughs.csv")
 
-district = pd.pivot_table(district, values='total_cases', index=["NAME"], columns=['date'], aggfunc=np.sum)
+district = pd.pivot_table(district, values='new_cases', index=["NAME"], columns=['date'], aggfunc=np.sum)
 
 
 
-london = gpd.read_file(r"/Users/giorgiosala/Documents/pe/map/londonb.shp")
+london = gpd.read_file(r"/Users/giorgiosala/Documents/master/London Covid Cases/london.shp")
 london = london.drop(columns=["NAME","GSS_CODE","NONLD_AREA","LAGSSCODE","HECTARES"])
 """
 #search for names that doesen't match between CSV file and SHP file ( I changed the not matching names in the CSV file )
@@ -56,21 +62,23 @@ for index,row in district.iterrows():
 #Merging "district" with "london" geopandas geodataframe
 merge = london.join(district, on="DISTRICT", how="right")
 
-for dates in merge.columns.to_list()[2:]:
+for dates in merge.columns.to_list()[2:3]:
     #Plot
+    print(dates)
     ax= merge.plot(column = dates,
-                   figsize=(20,20),
+                   figsize=(15,13),
                    vmin = 0,
-                   vmax = 35000,
+                   vmax = 1100,
                    cmap='OrRd', 
                    linewidth=0.1,
                    legend_kwds={'shrink': 0.5,},
                    edgecolor='black', 
                    legend=True
+                   
                    )
     
     #assign name to shapes
-    merge.apply(lambda x: ax.annotate(s=x.DISTRICT, xy=x.geometry.centroid.coords[0], ha='center'),axis=1);
+    merge.apply(lambda x: ax.annotate(text=x.DISTRICT, xy=x.geometry.centroid.coords[0], ha='center'),axis=1);
     
     ax.set_axis_off()
     ax.set_title("Total Covid Cases per London District \n\n"+ dates, fontsize=30 )
@@ -84,7 +92,7 @@ for dates in merge.columns.to_list()[2:]:
     f.seek(0)
     image_frame.append(PIL.Image.open(f))
 
-
+"""
 #create a gif
 image_frame[0].save("Dinamic covid 19 map of london.gif",format="GIF",
                      append_images= image_frame[1:],
@@ -93,7 +101,7 @@ image_frame[0].save("Dinamic covid 19 map of london.gif",format="GIF",
                      loop=1,
                      )
 
-
+"""
 
 f.close()
 
