82443b4ab4ab51c452fd8cd26d9ca2f2cd189201
Giorgio Sala <78495487+giorgiosala@users.noreply.github.com>
2021-03-03 15:57:41 +0000

    Covid map with not correct bins

--- London Covid Cases/london.py
+++ London Covid Cases/london.py
@@ -34,16 +34,17 @@ import pandas as pd
 import numpy as np
 import PIL
 import io
+import cv2
 
 image_frame=[]
-#Read the CSV file and privot the table to easier analisis
+#Read the CSV file and pivot the table to easier analisis
 district= pd.read_csv("phe_cases_london_boroughs.csv")
 
 district = pd.pivot_table(district, values='new_cases', index=["NAME"], columns=['date'], aggfunc=np.sum)
 
 
 
-london = gpd.read_file(r"/Users/giorgiosala/Documents/master/London Covid Cases/london.shp")
+london = gpd.read_file(r"/Users/giorgiosala/Documents/master/London Covid Cases/l.shp")
 london = london.drop(columns=["NAME","GSS_CODE","NONLD_AREA","LAGSSCODE","HECTARES"])
 """
 #search for names that doesen't match between CSV file and SHP file ( I changed the not matching names in the CSV file )
@@ -62,33 +63,31 @@ for index,row in district.iterrows():
 #Merging "district" with "london" geopandas geodataframe
 merge = london.join(district, on="DISTRICT", how="right")
 
-for dates in merge.columns.to_list()[2:3]:
+for dates in merge.columns.to_list()[300:301]:
     #Plot
     print(dates)
-    ax= merge.plot(column = dates,
-                   figsize=(15,13),
-                   vmin = 0,
-                   vmax = 1100,
+    ax= merge.plot(column=dates,
+                   figsize=[15,15], 
                    cmap='OrRd', 
-                   linewidth=0.1,
-                   legend_kwds={'shrink': 0.5,},
-                   edgecolor='black', 
-                   legend=True
-                   
+                   legend=True,
+                   scheme='UserDefined', 
+                   classification_kwds={'bins':[10,20,40,60,80,100,200,400,600,800,1000]},
+                   edgecolor="black",
+                   linewidth=0.4
                    )
     
     #assign name to shapes
     merge.apply(lambda x: ax.annotate(text=x.DISTRICT, xy=x.geometry.centroid.coords[0], ha='center'),axis=1);
     
     ax.set_axis_off()
-    ax.set_title("Total Covid Cases per London District \n\n"+ dates, fontsize=30 )
+    ax.set_title("New Covid Cases per London District \n\n"+ dates, fontsize=30 )
     
     
     
     img= ax.get_figure()
     
     f= io.BytesIO()
-    img.savefig(f, format="png")
+    img.savefig(f, format="png",bbox_inches="tight")
     f.seek(0)
     image_frame.append(PIL.Image.open(f))
 
@@ -98,10 +97,11 @@ image_frame[0].save("Dinamic covid 19 map of london.gif",format="GIF",
                      append_images= image_frame[1:],
                      save_all= True,
                      duration=300,
-                     loop=1,
+                     loop=0,
                      )
 
 """
+
 
 f.close()
 
